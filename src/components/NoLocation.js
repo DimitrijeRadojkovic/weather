642be8996b16b8ffe6068bc7fc0ce0474e7b812a
Dimitrije Radojkovic <dimitrijeradojkovic8@gmail.com>
2023-05-13 23:26:13 +0200

    promenjeni font, border i dodata animacija(NoLoc)

--- src/components/NoLocation.js
+++ src/components/NoLocation.js
@@ -7,8 +7,8 @@ const NoLocation = () => {
         <h1 className="text-3xl md:text-6xl text-center">Uh, oh - Something went wrong</h1>
 
         <form id="locationform" className="w-full flex flex-col items-center">
-            <motion.input type="text" placeholder="Enter location..." className="m-2 bg-gray-50 border border-black text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></motion.input>
-            <input type="submit" value="Submit" className="m-2 cursor-pointer bg-gray-50 border border-black text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block md:w-1/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
+            <motion.input whileFocus={{borderColor: "red"}} type="text" placeholder="Enter location..." className="m-2 bg-gray-50 text-gray-900 text-sm rounded-full shadow-md block w-full p-2.5"></motion.input>
+            <motion.input whileHover={{scale: 0.9}} type="submit" value="Submit" className="m-2 cursor-pointer bg-gray-50 text-gray-900 text-sm shadow-md rounded-full block md:w-1/4 p-2.5"></motion.input>
         </form>
             
     </motion.div>
